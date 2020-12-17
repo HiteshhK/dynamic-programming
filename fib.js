f596413b22ba15dff98aeb044be3250588a9b45d
Detesh.K <kaushikhitesh002@gmail.com>
2020-12-17 13:07:17 +0530

    first commit

---
+++ fib.js
@@ -0,0 +1,22 @@
+//without memoization
+/*const fib=(n)=>{
+    // if(n==3){
+    //     console.log(n);
+    // }
+    if(n <= 2) return 1;
+    return fib(n-1)+fib(n-2);
+}*/
+
+//with memoization
+//js object, keys will be arg to fn, value will be return value
+const fib=((n,memo={})=>{
+    if(n in memo) return memo[n];
+    if(n<=2) return 1;
+    memo[n]=fib(n-1,memo)+fib(n-2,memo);
+    return memo[n];
+
+})
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
